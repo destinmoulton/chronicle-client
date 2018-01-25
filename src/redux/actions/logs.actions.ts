0aa3bfcb1d43c1845fb264c679c5770728cc6554
Destin Moulton <destin.moulton@gmail.com>
2018-01-25 14:22:02 -0700

    Added methods to process and fire the reducer after the items have been processed.

--- src/redux/actions/logs.actions.ts
+++ src/redux/actions/logs.actions.ts
@@ -1,7 +1,11 @@
+import { Map } from "immutable";
+
 import { API_URL } from "../../../chronicle.config";
-import { LOGS_START_LOADING } from "../actionTypes";
+import * as ActionTypes from "../actionTypes";
 import * as Types from "../../common/types";
 import { JSON_HEADERS } from "../../common/headers";
+
+import { chronicleAPIQueryBuilder } from "../../lib/chronicleAPIQueryBuilder";
 
 export const loadLogs = () => {
     return (dispatch: Types.IDispatch) => {
@@ -12,7 +16,7 @@ export const loadLogs = () => {
 
 const setLoadingState = () => {
     return {
-        type: LOGS_START_LOADING
+        type: ActionTypes.LOGS_START_LOADING
     };
 };
 
@@ -22,14 +26,43 @@ const getServerLogs = () => {
         const fetchParams = {
             method: "POST",
             headers: JSON_HEADERS,
-            body: JSON.stringify(query)
+            body: chronicleAPIQueryBuilder({ app: "Nic Cage App", type: "log" })
         };
-        fetch(API_URL, fetchParams)
+        fetch(API_URL + "/query", fetchParams)
             .then(res => {
                 return res.json();
             })
-            .then(logs => {
-                console.log(logs);
+            .then(data => {
+                if (data.Items !== undefined) {
+                    dispatch(prepareAppLogs(data));
+                } else {
+                    console.error(
+                        "logs.actions :: getServerLogs() :: No Items in data.",
+                        data
+                    );
+                }
             });
+    };
+};
+
+const prepareAppLogs = (data: any) => {
+    return (dispatch: Types.IDispatch, getState: Types.IGetState) => {
+        // Build a map of the Items
+        let mappedItems: Types.IAppLogs = Map();
+        data.Items.forEach((item: any) => {
+            mappedItems = mappedItems.set(item.id, item);
+        });
+
+        //Merge with current data
+        const appLogs = getState().logs.appLogs;
+        const newAppLogs = appLogs.merge(mappedItems);
+        dispatch(setLoadedAppLogs(newAppLogs));
+    };
+};
+
+const setLoadedAppLogs = (appLogs: Map<string, any>) => {
+    return {
+        type: ActionTypes.LOGS_APP_DATA_LOADED,
+        appLogs
     };
 };
