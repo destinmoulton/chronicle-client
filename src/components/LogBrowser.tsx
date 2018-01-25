50bd5569a0604e8590a4007c8e7318c97c29d6a9
Destin Moulton <destin.moulton@gmail.com>
2018-01-25 13:13:14 -0700

    Converted LogBrowser to a stateless component.

--- src/components/LogBrowser.tsx
+++ src/components/LogBrowser.tsx
@@ -2,15 +2,15 @@ import * as React from "react";
 
 import { Row, Col } from "antd";
 
-class LogBrowser extends React.Component {
-    render() {
-        return (
-            <Row>
-                <Col span={4}>Menu</Col>
-                <Col span={20}>Main Content</Col>
-            </Row>
-        );
-    }
-}
+import LogList from "./LogList/LogList";
+
+const LogBrowser: React.SFC = props => (
+    <Row>
+        <Col span={4}>
+            <LogList />
+        </Col>
+        <Col span={20}>Main Content</Col>
+    </Row>
+);
 
 export default LogBrowser;
