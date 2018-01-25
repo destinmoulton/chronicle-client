113d4d4f5110922daa7b0bb8002a7866d6c110cf
Destin Moulton <destin.moulton@gmail.com>
2018-01-25 11:15:20 -0700

    Added thunk middleware.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,6 @@
 import { routerReducer, routerMiddleware } from "react-router-redux";
 import { combineReducers, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import createHistory from "history/createBrowserHistory";
 
 const history = createHistory();
@@ -8,7 +9,7 @@ const store = createStore(
     combineReducers({
         routerReducer
     }),
-    applyMiddleware(routerMiddleware(history))
+    applyMiddleware(routerMiddleware(history), thunk)
 );
 
 export { history, store };
