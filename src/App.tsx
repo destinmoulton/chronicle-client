3b699d50f1756534b001a6bc9527256504da1107
Destin Moulton <destin.moulton@gmail.com>
2018-01-25 09:53:40 -0700

    Modified route to point to the Log Browser.

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,28 @@
 import * as React from "react";
-import { connect } from "react-redux";
-import { Route, Switch } from "react-router";
+import { Route } from "react-router";
 import { Link } from "react-router-dom";
 
 import { Layout } from "antd";
 const { Header, Content, Footer } = Layout;
 
-import { IConnectedSwitchState } from "./interfaces";
+import LogBrowser from "./components/LogBrowser";
 
-const ConnectedSwitch = connect((state: IConnectedSwitchState) => ({
-    location: state.location
-}))(Switch as any);
-
-const AppContainer = () => (
-    <ConnectedSwitch>
-        <Layout>
-            <Header>Chronicle Client</Header>
-            <Content>
-                <Route
-                    exact
-                    path="/"
-                    component={() => (
-                        <h1>
-                            Home <Link to="/about">About</Link>
-                        </h1>
-                    )}
-                />
-                <Route
-                    path="/about"
-                    component={() => (
-                        <h1>
-                            About <Link to="/">Home</Link>
-                        </h1>
-                    )}
-                />
-            </Content>
-            <Footer>MIT License</Footer>
-        </Layout>
-    </ConnectedSwitch>
+const App = () => (
+    <Layout>
+        <Header>Chronicle Client</Header>
+        <Content>
+            <Route exact path="/" component={LogBrowser} />
+            <Route
+                path="/about"
+                component={() => (
+                    <h1>
+                        About <Link to="/">Home</Link>
+                    </h1>
+                )}
+            />
+        </Content>
+        <Footer>MIT License</Footer>
+    </Layout>
 );
-
-const App = connect((state: IConnectedSwitchState) => ({
-    location: state.location
-}))(AppContainer);
 
 export default App;
