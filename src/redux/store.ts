7a5d266db3dd31c84ca2e947e761532fb64af743
Destin Moulton <destin.moulton@gmail.com>
2018-01-25 09:53:03 -0700

    Added Combine Reducers.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { routerReducer, routerMiddleware } from "react-router-redux";
-import { createStore, applyMiddleware } from "redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
 import createHistory from "history/createBrowserHistory";
 
 const history = createHistory();
 
 const store = createStore(
-    routerReducer,
+    combineReducers({
+        routerReducer
+    }),
     applyMiddleware(routerMiddleware(history))
 );
 
