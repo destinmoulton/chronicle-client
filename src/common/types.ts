76e08e3cae50ef211315a5c864d59092c7df63b8
Destin Moulton <destin.moulton@gmail.com>
2018-01-25 11:15:09 -0700

    Modified comments for the types.

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,12 +1,19 @@
-export type IThunkAction<R, S, E> = (
-    dispatch: IDispatch<S>,
-    getState: () => S,
-    extraArgument: E
-) => R;
+/** Redux Interfaces */
+// Redux dispatch()
+export interface IDispatch {
+    <R>(
+        asyncAction: (dispatch: IDispatch, getState: () => IRootStoreState) => R
+    ): R;
+    <R>(asyncAction: (dispatch: IDispatch) => R): R;
+    // (neverAction: (dispatch: Dispatch, getState: () => GetState) => never): never;
+    (action: object): void;
+    // (action: Thunk): ; // thunks in this app must return a promise
+}
 
-export interface IDispatch<S> {
-    <R, E>(asyncAction: IThunkAction<R, S, E>): R;
+// Redux getState()
+export interface IGetState {
+    (): IRootStoreState;
 }
-export interface IDispatch<S> {
-    <A>(action: A & { type: any }): A & { type: any };
-}
+
+/* Store States */
+export interface IRootStoreState {}
