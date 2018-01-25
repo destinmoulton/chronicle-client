cee3bbb8f9dcf77dd08e467de81bce1b8346a240
Destin Moulton <destin.moulton@gmail.com>
2018-01-25 13:14:53 -0700

    Added basic interfaces and redux mapping.

--- src/components/LogList/LogList.tsx
+++ src/components/LogList/LogList.tsx
@@ -1,10 +1,52 @@
 import * as React from "react";
+import { connect } from "react-redux";
+
+import * as Types from "../../common/types";
+
+import * as LogsActions from "../../redux/actions/logs.actions";
 
 import LoadingLogs from "./LoadingLogs";
-class LogList extends React.Component {
+
+interface IMapDispatchToProps {
+    loadLogs: () => void;
+}
+
+interface IMapStateToProps {
+    logsAreLoading: boolean;
+    logsHaveData: boolean;
+}
+interface ILogListProps extends IMapDispatchToProps, IMapStateToProps {}
+
+class LogList extends React.Component<ILogListProps> {
+    componentDidMount() {
+        const { loadLogs, logsAreLoading, logsHaveData } = this.props;
+
+        if (!logsAreLoading && !logsHaveData) {
+            loadLogs();
+        }
+    }
+
     render() {
-        return <LoadingLogs />;
+        const { logsAreLoading, logsHaveData } = this.props;
+
+        let loading = logsAreLoading ? <LoadingLogs /> : null;
+
+        return loading;
     }
 }
 
-export default LogList;
+const mapStateToProps = (state: Types.IRootStoreState): IMapStateToProps => {
+    const { logs } = state;
+    return {
+        logsAreLoading: logs.isLoading,
+        logsHaveData: logs.hasData
+    };
+};
+
+const mapDispatchToProps = (dispatch: Types.IDispatch): IMapDispatchToProps => {
+    return {
+        loadLogs: () => dispatch(LogsActions.loadLogs())
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LogList);
