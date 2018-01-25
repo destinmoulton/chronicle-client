3f49122d244173848cd469151f91e49710335281
Destin Moulton <destin.moulton@gmail.com>
2018-01-25 16:29:41 -0700

    Added click handler.

--- src/components/LogList/LogList.tsx
+++ src/components/LogList/LogList.tsx
@@ -17,7 +17,9 @@ interface IMapStateToProps {
     logsData: Types.IAppLogs;
     logsHaveData: boolean;
 }
-interface ILogListProps extends IMapDispatchToProps, IMapStateToProps {}
+interface ILogListProps extends IMapDispatchToProps, IMapStateToProps {
+    clickHandler: (logItem: Types.ILogItem) => void;
+}
 
 class LogList extends React.Component<ILogListProps> {
     componentDidMount() {
@@ -28,14 +30,27 @@ class LogList extends React.Component<ILogListProps> {
         }
     }
 
+    _clickHandler(item: Types.ILogItem) {
+        this.props.clickHandler(item);
+    }
+
     render() {
-        const { logsAreLoading, logsData, logsHaveData } = this.props;
+        const {
+            clickHandler,
+            logsAreLoading,
+            logsData,
+            logsHaveData
+        } = this.props;
 
         let loading = logsAreLoading ? <LoadingLogs /> : null;
 
         let list: any[] = [];
         logsData.map((item, key) => {
-            list.push(<LogItem key={key} item={item} />);
+            list.push(
+                <div key={key} onClick={this._clickHandler.bind(this, item)}>
+                    <LogItem item={item} />
+                </div>
+            );
         });
         return (
             <div>
