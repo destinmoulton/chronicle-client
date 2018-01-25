7bd90e6c9136850e84b1961923506ba8668d968e
Destin Moulton <destin.moulton@gmail.com>
2018-01-25 11:31:37 -0700

    Create log list to hold the list of logs.

---
+++ src/components/LogList/LogList.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+import LoadingLogs from "./LoadingLogs";
+class LogList extends React.Component {
+    render() {
+        return <LoadingLogs />;
+    }
+}
+
+export default LogList;
