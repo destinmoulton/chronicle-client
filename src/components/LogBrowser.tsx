87ba384117b4dab937e0b80ff712e5047e7b5366
Destin Moulton <destin.moulton@gmail.com>
2018-01-25 16:29:12 -0700

    Added click handler.

--- src/components/LogBrowser.tsx
+++ src/components/LogBrowser.tsx
@@ -2,15 +2,47 @@ import * as React from "react";
 
 import { Row, Col } from "antd";
 
+import * as Types from "../common/types";
+import LogExplorer from "./LogExplorer";
 import LogList from "./LogList/LogList";
 
-const LogBrowser: React.SFC = props => (
-    <Row>
-        <Col span={4}>
-            <LogList />
-        </Col>
-        <Col span={20}>Main Content</Col>
-    </Row>
-);
+interface ILogBrowserProps {}
+interface ILogBrowserState {
+    _activeLogItem: undefined | Types.ILogItem;
+}
+class LogBrowser extends React.Component<ILogBrowserProps, ILogBrowserState> {
+    constructor(props: ILogBrowserProps) {
+        super(props);
+
+        this.state = {
+            _activeLogItem: undefined
+        };
+
+        this._handleClickLogItem = this._handleClickLogItem.bind(this);
+    }
+
+    _handleClickLogItem = (logItem: Types.ILogItem) => {
+        this.setState({
+            _activeLogItem: logItem
+        });
+    };
+
+    render() {
+        const { _activeLogItem } = this.state;
+        const logExplorer =
+            _activeLogItem !== undefined ? (
+                <LogExplorer item={_activeLogItem} />
+            ) : null;
+
+        return (
+            <Row>
+                <Col span={4}>
+                    <LogList clickHandler={this._handleClickLogItem} />
+                </Col>
+                <Col span={20}>{logExplorer}</Col>
+            </Row>
+        );
+    }
+}
 
 export default LogBrowser;
