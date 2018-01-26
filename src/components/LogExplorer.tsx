7eb1801d4ec99c7204394db1ec7f00caac0d66b9
Destin Moulton <destin.moulton@gmail.com>
2018-01-26 15:49:41 -0700

    Added light "google" base16 theme to the LogExplorer.

--- src/components/LogExplorer.tsx
+++ src/components/LogExplorer.tsx
@@ -7,13 +7,35 @@ import * as Types from "../common/types";
 interface ILogExplorerProps {
     item: Types.ILogItem;
 }
+
+const theme = {
+    scheme: "google",
+    author: "seth wright (http://sethawright.com)",
+    base00: "#1d1f21",
+    base01: "#282a2e",
+    base02: "#373b41",
+    base03: "#969896",
+    base04: "#b4b7b4",
+    base05: "#c5c8c6",
+    base06: "#e0e0e0",
+    base07: "#ffffff",
+    base08: "#CC342B",
+    base09: "#F96A38",
+    base0A: "#FBA922",
+    base0B: "#198844",
+    base0C: "#3971ED",
+    base0D: "#3971ED",
+    base0E: "#A36AC7",
+    base0F: "#3971ED"
+};
+
 const LogExplorer: React.SFC<ILogExplorerProps> = (
     props: ILogExplorerProps
 ) => {
     console.log(props.item);
     return (
         <div>
-            <JSONTree data={props.item} />
+            <JSONTree data={props.item} theme={theme} />
         </div>
     );
 };
