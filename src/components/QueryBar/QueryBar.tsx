620c7d8c27fddf77e75a7fa377bfdf51275b346e
Destin Moulton <destin.moulton@gmail.com>
2018-01-29 16:41:09 -0700

    Added Sort By component to the query bar.

--- src/components/QueryBar/QueryBar.tsx
+++ src/components/QueryBar/QueryBar.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 
 import DateRange from "./DateRange";
+import SortBy from "./SortBy";
 
 interface IQueryBarProps {}
 const QueryBar = (props: IQueryBarProps) => {
     return (
         <div className="chc-query-bar">
+            <SortBy />
             <DateRange />
         </div>
     );
