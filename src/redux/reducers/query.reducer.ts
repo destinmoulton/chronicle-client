c93ea386c6e866a7bfebec5509541c696d3e1742
Destin Moulton <destin.moulton@gmail.com>
2018-01-29 14:14:24 -0700

    Wrapped the times.

--- src/redux/reducers/query.reducer.ts
+++ src/redux/reducers/query.reducer.ts
@@ -2,21 +2,26 @@ import * as moment from "moment";
 
 import * as ActionTypes from "../actionTypes";
 import * as Types from "../../common/types";
+import { getDateBeginPoint, getDateEndPoint } from "../../lib/dateRangePoints";
 import { DATE_FORMAT, NUMBER_MONTHS_PAST } from "../../common/date.constants";
 
 const INITIAL_STATE: Types.IReducerQueryState = {
-    dateRangeStart: moment().subtract(NUMBER_MONTHS_PAST, "months"),
-    dateRangeEnd: moment()
+    dateRangeStart: getDateBeginPoint(
+        moment().subtract(NUMBER_MONTHS_PAST, "months") // Some number months ago
+    ),
+    dateRangeEnd: getDateEndPoint(moment()) // Today
 };
 
 const queryReducer = (state = INITIAL_STATE, action: Types.IQueryAction) => {
     switch (action.type) {
         case ActionTypes.QUERY_SET_DATE_START:
+            console.log("setting START date");
             return {
                 ...state,
                 dateRangeStart: action.date
             };
         case ActionTypes.QUERY_SET_DATE_END:
+            console.log("setting END date");
             return {
                 ...state,
                 dateRangeEnd: action.date
