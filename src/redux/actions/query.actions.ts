df0d340266bc37ac492eab36b7ead55e944bca5d
Destin Moulton <destin.moulton@gmail.com>
2018-01-29 14:14:07 -0700

    Added additional dispatches for loading the logs after the dates are set.

--- src/redux/actions/query.actions.ts
+++ src/redux/actions/query.actions.ts
@@ -1,17 +1,37 @@
 import { Moment } from "moment";
 
+import * as Types from "../../common/types";
+
 import * as ActionTypes from "../actionTypes";
+import { IDispatch } from "../../common/types";
+import { getDateBeginPoint, getDateEndPoint } from "../../lib/dateRangePoints";
+
+import { loadLogs } from "../actions/logs.actions";
 
 export const setQueryDateStart = (startMoment: Moment) => {
+    return (dispatch: IDispatch) => {
+        dispatch(overwriteQueryDateStart(startMoment));
+        dispatch(loadLogs());
+    };
+};
+
+const overwriteQueryDateStart = (startMoment: Moment) => {
     return {
         type: ActionTypes.QUERY_SET_DATE_START,
-        date: startMoment
+        date: getDateBeginPoint(startMoment)
     };
 };
 
 export const setQueryDateEnd = (endMoment: Moment) => {
+    return (dispatch: IDispatch) => {
+        dispatch(overwriteQueryDateEnd(endMoment));
+        dispatch(loadLogs());
+    };
+};
+
+const overwriteQueryDateEnd = (endMoment: Moment) => {
     return {
         type: ActionTypes.QUERY_SET_DATE_END,
-        date: endMoment
+        date: getDateEndPoint(endMoment)
     };
 };
