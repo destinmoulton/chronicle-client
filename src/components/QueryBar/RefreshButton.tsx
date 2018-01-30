68c2748e860b9c67cbb4a7cb25e4c5e0c87f6d78
Destin Moulton <destin.moulton@gmail.com>
2018-01-30 11:46:44 -0700

    Created Refresh Button component for the query bar.

---
+++ src/components/QueryBar/RefreshButton.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { connect } from "react-redux";
+
+import { Button } from "antd";
+
+import * as Types from "../../common/types";
+
+import { loadLogs } from "../../redux/actions/logs.actions";
+
+interface IMapStateToProps {}
+
+interface IMapDispatchToProps {
+    loadLogs: () => void;
+}
+interface IRefreshButtonProps extends IMapStateToProps, IMapDispatchToProps {}
+
+class RefreshButton extends React.Component<IRefreshButtonProps> {
+    constructor(props: IRefreshButtonProps) {
+        super(props);
+
+        this._handlePressReload = this._handlePressReload.bind(this);
+    }
+
+    _handlePressReload() {
+        this.props.loadLogs();
+    }
+
+    render() {
+        return (
+            <Button
+                shape="circle"
+                icon="reload"
+                onClick={this._handlePressReload}
+            />
+        );
+    }
+}
+
+const mapStateToProps = (state: Types.IRootStoreState): IMapStateToProps => {
+    return {};
+};
+
+const mapDispatchToProps = (dispatch: Types.IDispatch): IMapDispatchToProps => {
+    return {
+        loadLogs: () => dispatch(loadLogs())
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RefreshButton);
