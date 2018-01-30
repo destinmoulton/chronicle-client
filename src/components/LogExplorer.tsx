bb88bf784d514d3cf69e07106d5fccbc2c9b4051
Destin Moulton <destin.moulton@gmail.com>
2018-01-30 13:54:03 -0700

    Changed the name of the css class.

--- src/components/LogExplorer.tsx
+++ src/components/LogExplorer.tsx
@@ -4,37 +4,18 @@ import JSONTree from "react-json-tree";
 
 import * as Types from "../common/types";
 
+import THEME from "../common/base16.theme";
+
 interface ILogExplorerProps {
     item: Types.ILogItem;
 }
-
-const theme = {
-    scheme: "google",
-    author: "seth wright (http://sethawright.com)",
-    base00: "#1d1f21",
-    base01: "#282a2e",
-    base02: "#373b41",
-    base03: "#969896",
-    base04: "#b4b7b4",
-    base05: "#c5c8c6",
-    base06: "#e0e0e0",
-    base07: "#ffffff",
-    base08: "#CC342B",
-    base09: "#F96A38",
-    base0A: "#FBA922",
-    base0B: "#198844",
-    base0C: "#3971ED",
-    base0D: "#3971ED",
-    base0E: "#A36AC7",
-    base0F: "#3971ED"
-};
 
 const LogExplorer: React.SFC<ILogExplorerProps> = (
     props: ILogExplorerProps
 ) => {
     return (
-        <div className="chc-log-browser-json-tree-container">
-            <JSONTree data={props.item} theme={theme} hideRoot />
+        <div className="chc-log-explorer-json-tree-container">
+            <JSONTree data={props.item} theme={THEME} hideRoot />
         </div>
     );
 };
