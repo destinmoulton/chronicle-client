5f0f7749e7187b0363af03223105b9dc682cb688
Destin Moulton <destin.moulton@gmail.com>
2018-01-30 14:02:03 -0700

    Added style for the main info box.

--- src/components/LogExplorer.tsx
+++ src/components/LogExplorer.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 
 import JSONTree from "react-json-tree";
+import { Icon } from "antd";
 
 import * as Types from "../common/types";
 
 import THEME from "../common/base16.theme";
+import { LOG_ICONS } from "../common/icons.constants";
 
 interface ILogExplorerProps {
     item: Types.ILogItem;
@@ -13,9 +15,18 @@ interface ILogExplorerProps {
 const LogExplorer: React.SFC<ILogExplorerProps> = (
     props: ILogExplorerProps
 ) => {
+    const { item } = props;
+
+    const icon = LOG_ICONS[item.type] || { icon: "", color: "" };
+
     return (
-        <div className="chc-log-explorer-json-tree-container">
-            <JSONTree data={props.item} theme={THEME} hideRoot />
+        <div>
+            <div className="chc-log-explorer-main-box">
+                <Icon type={icon.icon} />
+            </div>
+            <div className="chc-log-explorer-json-tree-container">
+                <JSONTree data={item} theme={THEME} hideRoot />
+            </div>
         </div>
     );
 };
