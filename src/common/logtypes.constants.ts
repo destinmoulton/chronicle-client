ebcf52e9d2ca92c2768b198d5666d156a35df727
Destin Moulton <destin.moulton@gmail.com>
2018-01-30 14:01:42 -0700

    Renamed icons.constants to logtypes.constants. Added name fields to each log type.

---
+++ src/common/logtypes.constants.ts
@@ -0,0 +1,61 @@
+/**
+ * Define details for the different log types.
+ */
+
+export interface ILogType {
+    icon: string;
+    color: string;
+    name: string;
+}
+
+interface ILogTypes {
+    [key: string]: ILogType;
+}
+
+export const LOG_TYPES: ILogTypes = {
+    assert: {
+        icon: "notification",
+        color: "#cc0000",
+        name: "assert()"
+    },
+    error: {
+        icon: "exclamation-circle",
+        color: "#cc0000",
+        name: "error()"
+    },
+    group: {
+        icon: "folder",
+        color: "#ffc049",
+        name: "group()"
+    },
+    info: {
+        icon: "info-circle",
+        color: "#058ed9",
+        name: "info()"
+    },
+    log: {
+        icon: "bars",
+        color: "#058ed9",
+        name: "log()"
+    },
+    table: {
+        icon: "table",
+        color: "#058ed9",
+        name: "table()"
+    },
+    time: {
+        icon: "clock-circle-o",
+        color: "#158700",
+        name: "time()"
+    },
+    trace: {
+        icon: "camera-o",
+        color: "#058ed9",
+        name: "trace()"
+    },
+    warn: {
+        icon: "warning",
+        color: "#ff8202",
+        name: "warn()"
+    }
+};
