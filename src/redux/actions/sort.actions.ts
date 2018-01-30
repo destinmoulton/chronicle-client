29883688da437fae7516f59a39d58df16cc4c067
Destin Moulton <destin.moulton@gmail.com>
2018-01-30 10:15:32 -0700

    Fixed misnamed action to type.

--- src/redux/actions/sort.actions.ts
+++ src/redux/actions/sort.actions.ts
@@ -2,7 +2,7 @@ import * as ActionTypes from "../actionTypes";
 
 export const setSortOrder = (newOrder: string) => {
     return {
-        action: ActionTypes.SORT_SET_SORT_ORDER,
+        type: ActionTypes.SORT_SET_SORT_ORDER,
         order: newOrder
     };
 };
