35aef0ab234576d3aa53b0ed64ad265e12528e5c
Destin Moulton <destin.moulton@gmail.com>
2018-01-30 14:41:20 -0700

    Converted stateless method into class and added isExploring state variable.

--- src/components/LogList/LogItem.tsx
+++ src/components/LogList/LogItem.tsx
@@ -13,28 +13,55 @@ interface ILogItemProps {
     activeLogItemId: string;
     item: Types.ILogItem;
 }
-const LogItem: React.SFC<ILogItemProps> = (props: ILogItemProps) => {
-    const { activeLogItemId, item } = props;
-    const { createdAt, type } = item;
 
-    const time = moment(createdAt).format("MMM D, Y h:mm a");
+interface ILogItemState {
+    isExploring: boolean;
+}
+class LogItem extends React.Component<ILogItemProps, ILogItemState> {
+    constructor(props: ILogItemProps) {
+        super(props);
 
-    const logTypeObj: ILogType = LOG_TYPES[type] || LOG_TYPE_EMPTY;
+        this.setState({
+            isExploring: false
+        });
 
-    const activeClass =
-        activeLogItemId === item.id ? "chc-log-list-item-active" : "";
-    return (
-        <div className={"chc-log-list-item " + activeClass}>
-            <div className="chc-log-list-item-type">
-                <Icon
-                    type={logTypeObj.icon}
-                    style={{ color: logTypeObj.color }}
-                />&nbsp;
-                {logTypeObj.name}
+        this._toggleExploring = this._toggleExploring.bind(this);
+    }
+
+    _toggleExploring() {
+        this.setState({ isExploring: !this.state.isExploring });
+    }
+
+    render() {
+        const { isExploring } = this.state;
+        const { activeLogItemId, item } = this.props;
+        const { createdAt, type } = item;
+
+        const time = moment(createdAt).format("MMM D, Y h:mm a");
+
+        const logTypeObj: ILogType = LOG_TYPES[type] || LOG_TYPE_EMPTY;
+
+        const activeClass =
+            activeLogItemId === item.id ? "chc-log-list-item-active" : "";
+
+        const exploringCaret = isExploring ? "caret-down" : "caret-right";
+        return (
+            <div className={"chc-log-list-item " + activeClass}>
+                <div className="chc-log-list-item-type">
+                    <Icon
+                        type={exploringCaret}
+                        onClick={this._toggleExploring}
+                    />&nbsp;
+                    <Icon
+                        type={logTypeObj.icon}
+                        style={{ color: logTypeObj.color }}
+                    />&nbsp;
+                    {logTypeObj.name}
+                </div>
+                <div className="chc-log-list-item-time">{time}</div>
             </div>
-            <div className="chc-log-list-item-time">{time}</div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default LogItem;
