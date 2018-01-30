90b49ea4916b6cb91b24e5b86239bd741f55f6ca
Destin Moulton <destin.moulton@gmail.com>
2018-01-30 10:14:18 -0700

    Removed field from the comparators and added error handling for invalid comparator order.

--- src/lib/comparators.ts
+++ src/lib/comparators.ts
@@ -1,8 +1,18 @@
+/**
+ * Return the comparator that should be run.
+ *
+ * @param order string
+ */
 export const comparatorDispatch = (order: string) => {
     if (order === "desc") {
         return comparatorDesc;
-    } else {
+    } else if (order === "asc") {
         return comparatorAsc;
+    } else {
+        console.error(
+            "comparators.js :: comparatorDispatch() :: Invalid comparator selected.",
+            order
+        );
     }
 };
 
@@ -12,9 +22,9 @@ export const comparatorDispatch = (order: string) => {
  * @param b
  * @param field
  */
-export const comparatorDesc = (a: any, b: any, field: string) => {
-    if (a[field] > b[field]) return 1;
-    if (a[field] < b[field]) return -1;
+export const comparatorDesc = (a: any, b: any) => {
+    if (a > b) return -1;
+    if (a < b) return 1;
     return 0;
 };
 
@@ -24,8 +34,8 @@ export const comparatorDesc = (a: any, b: any, field: string) => {
  * @param b
  * @param field
  */
-export const comparatorAsc = (a: any, b: any, field: string) => {
-    if (a[field] < b[field]) return 1;
-    if (a[field] > b[field]) return -1;
+export const comparatorAsc = (a: any, b: any) => {
+    if (a < b) return -1;
+    if (a > b) return 1;
     return 0;
 };
