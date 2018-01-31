082e254f5292721ec2b404389dcd40942e5f4da5
Destin Moulton <destin.moulton@gmail.com>
2018-01-31 13:14:30 -0700

    Show string without tree.

--- src/components/LogList/LogItemDetails.tsx
+++ src/components/LogList/LogItemDetails.tsx
@@ -16,9 +16,16 @@ const LogItemExplorer: React.SFC<ILogItemExplorerProps> = (
 ) => {
     const { item } = props;
 
+    let details = null;
+
+    if (typeof item.info === "string") {
+        details = item.info;
+    } else {
+        details = <JSONTree data={item.info} theme={THEME} hideRoot />;
+    }
     return (
         <div>
-            <div className="chc-log-item-explorer-main-box" />
+            <div className="chc-log-item-explorer-main-box">{details}</div>
             <div className="chc-log-item-explorer-json-tree-container">
                 <JSONTree data={item} theme={THEME} hideRoot />
             </div>
