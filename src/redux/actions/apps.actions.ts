34c420e794ccbbb02c12ae5895e0193f2c6c5174
Destin Moulton <destin.moulton@gmail.com>
2018-01-31 16:28:07 -0700

    Added apps actions file with fetch, begin, and complete methods.

---
+++ src/redux/actions/apps.actions.ts
@@ -0,0 +1,57 @@
+import { List } from "immutable";
+import { API_URL } from "../../../chronicle.config";
+import * as ActionTypes from "../actionTypes";
+import * as Types from "../../common/types";
+
+import { JSON_HEADERS } from "../../common/headers";
+
+export const getApps = () => {
+    return (dispatch: Types.IDispatch, getState: Types.IGetState) => {
+        dispatch(setAppCallBegin());
+        dispatch(apiGetApps());
+    };
+};
+
+const setAppCallBegin = (): Types.IAppAction => {
+    return {
+        type: ActionTypes.APPS_CALL_BEGIN
+    };
+};
+
+const apiGetApps = () => {
+    return (dispatch: Types.IDispatch) => {
+        const url = API_URL + "/apps";
+        const params = {
+            method: "GET",
+            headers: JSON_HEADERS
+        };
+
+        fetch(url, params)
+            .then(res => {
+                return res.json();
+            })
+            .then((apps: any) => {
+                dispatch(setAppCallComplete());
+                dispatch(prepareData(apps.apps));
+            });
+    };
+};
+
+const setAppCallComplete = (): Types.IAppAction => {
+    return {
+        type: ActionTypes.APPS_CALL_COMPLETE
+    };
+};
+
+const prepareData = (apps: Types.IAppsRawList) => {
+    return (dispatch: Types.IDispatch) => {
+        dispatch(writeAppData(List.of(...apps)));
+    };
+};
+
+const writeAppData = (data: Types.IAppsList): Types.IAppAction => {
+    return {
+        type: ActionTypes.APPS_WRITE_DATA,
+        data
+    };
+};
