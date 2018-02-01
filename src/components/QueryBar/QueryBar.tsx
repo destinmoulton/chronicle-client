0890de9f5c6a73b03e381fbb02eef002147eb671
Destin Moulton <destin.moulton@gmail.com>
2018-01-31 17:11:10 -0700

    Added selectedApp mapping.

--- src/components/QueryBar/QueryBar.tsx
+++ src/components/QueryBar/QueryBar.tsx
@@ -1,18 +1,33 @@
 import * as React from "react";
+import { connect } from "react-redux";
+
+import * as Types from "../../common/types";
 
 import DateRange from "./DateRange";
 import RefreshButton from "./RefreshButton";
 import SortBy from "./SortBy";
 
-interface IQueryBarProps {}
-const QueryBar = (props: IQueryBarProps) => {
-    return (
-        <div className="chc-query-bar">
-            <SortBy />
-            <DateRange />
-            <RefreshButton />
-        </div>
-    );
-};
+interface IMapStateToProps {
+    selectedApp: string;
+}
+interface IQueryBarProps extends IMapStateToProps {}
+class QueryBar extends React.Component<IQueryBarProps> {
+    render() {
+        return (
+            <div className="chc-query-bar">
+                <strong>{this.props.selectedApp}</strong>
+                <SortBy />
+                <DateRange />
+                <RefreshButton />
+            </div>
+        );
+    }
+}
 
-export default QueryBar;
+const mapStateToProps = (state: Types.IRootStoreState): IMapStateToProps => {
+    const { query } = state;
+    return {
+        selectedApp: query.selectedApp
+    };
+};
+export default connect(mapStateToProps)(QueryBar);
