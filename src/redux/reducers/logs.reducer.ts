d28082787b4cebe99789d6f9f3febbb8ce523cc0
Destin Moulton <destin.moulton@gmail.com>
2018-01-31 17:11:52 -0700

    Added reduction to clear the data.

--- src/redux/reducers/logs.reducer.ts
+++ src/redux/reducers/logs.reducer.ts
@@ -5,29 +5,40 @@ import * as Types from "../../common/types";
 const INITIAL_STATE: Types.IReducerLogsState = {
     isLoading: false,
     hasData: false,
-    appLogs: OrderedMap<string, any>()
+    appLogs: OrderedMap<string, Types.ILogItem>()
 };
 
 export default (state = INITIAL_STATE, action: Types.ILogAction) => {
     switch (action.type) {
-        case ActionTypes.LOGS_API_CALL_BEGIN:
+        case ActionTypes.LOGS_CLEAR_DATA: {
+            return {
+                ...state,
+                isLoading: false,
+                hasData: false,
+                appLogs: OrderedMap<string, Types.ILogItem>()
+            };
+        }
+        case ActionTypes.LOGS_API_CALL_BEGIN: {
             return {
                 ...state,
                 isLoading: true,
                 hasData: false,
-                appLogs: OrderedMap<string, any>()
+                appLogs: OrderedMap<string, Types.ILogItem>()
             };
-        case ActionTypes.LOGS_API_CALL_COMPLETE:
+        }
+        case ActionTypes.LOGS_API_CALL_COMPLETE: {
             return {
                 ...state,
                 isLoading: false
             };
-        case ActionTypes.LOGS_WRITE_DATA:
+        }
+        case ActionTypes.LOGS_WRITE_DATA: {
             return {
                 ...state,
                 hasData: true,
                 appLogs: action.data
             };
+        }
         default:
             return { ...state };
     }
