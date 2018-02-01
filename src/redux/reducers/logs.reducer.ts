f641ef3160756d7ebf34e5ac4afe64e97cf331fe
Destin Moulton <destin.moulton@gmail.com>
2018-02-01 16:47:02 -0700

    Added reduction for set app log types.

--- src/redux/reducers/logs.reducer.ts
+++ src/redux/reducers/logs.reducer.ts
@@ -1,11 +1,12 @@
-import { OrderedMap } from "immutable";
+import { List, OrderedMap } from "immutable";
 import * as ActionTypes from "../actionTypes";
 import * as Types from "../../common/types";
 
 const INITIAL_STATE: Types.IReducerLogsState = {
     isLoading: false,
     hasData: false,
-    appLogs: OrderedMap<string, Types.ILogItem>()
+    appLogs: OrderedMap<string, Types.ILogItem>(),
+    appLogTypes: List<string>()
 };
 
 export default (state = INITIAL_STATE, action: Types.ILogAction) => {
@@ -15,7 +16,8 @@ export default (state = INITIAL_STATE, action: Types.ILogAction) => {
                 ...state,
                 isLoading: false,
                 hasData: false,
-                appLogs: OrderedMap<string, Types.ILogItem>()
+                appLogs: OrderedMap<string, Types.ILogItem>(),
+                appLogTypes: List<string>()
             };
         }
         case ActionTypes.LOGS_API_CALL_BEGIN: {
@@ -37,6 +39,12 @@ export default (state = INITIAL_STATE, action: Types.ILogAction) => {
                 ...state,
                 hasData: true,
                 appLogs: action.data
+            };
+        }
+        case ActionTypes.LOGS_SET_APP_LOG_TYPES: {
+            return {
+                ...state,
+                appLogTypes: action.data
             };
         }
         default:
