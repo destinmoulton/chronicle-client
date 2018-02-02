bc23c8f059ea145792faf3a7f38b1cc04275a100
Destin Moulton <destin.moulton@gmail.com>
2018-02-02 15:05:41 -0700

    Added method to build the graphable data array.

--- src/lib/graphprep.ts
+++ src/lib/graphprep.ts
@@ -1,5 +1,8 @@
+import * as moment from "moment";
 import { Moment } from "moment";
 import { GRAPH_DATE_FORMAT } from "../common/date.constants";
+
+import * as Types from "../common/types";
 
 /**
  * Generate an array/series of dates including the
@@ -11,7 +14,7 @@ import { GRAPH_DATE_FORMAT } from "../common/date.constants";
 export const generateDateSeries = (
     startDate: Moment,
     endDate: Moment
-): string[] => {
+): Types.TDateSeries => {
     const datePointer = startDate.clone();
     const dates = [];
     while (!datePointer.isAfter(endDate, "day")) {
@@ -19,4 +22,50 @@ export const generateDateSeries = (
         datePointer.add(1, "d");
     }
     return dates;
+};
+
+export const buildGraphableDataArray = (
+    dateSeries: Types.TDateSeries,
+    appLogTypes: Types.TAppLogTypes,
+    appLogs: Types.TAppLogs
+) => {
+    interface ITypesTemp {
+        [key: string]: number;
+    }
+
+    const types = appLogTypes.toArray();
+    const typesInitial: ITypesTemp = {};
+    for (let i = 0; i < types.length; i++) {
+        // Initialize the count for each type to 0
+        typesInitial[types[i]] = 0;
+    }
+
+    let dateKeyedTypes: { [key: string]: ITypesTemp } = {};
+    dateSeries.map(date => {
+        // Build a date keyed object of the types count
+        // so far initialized to 0
+        dateKeyedTypes[date] = { ...typesInitial };
+    });
+
+    appLogs.map((item: Types.ILogItem) => {
+        const timeOfLog = moment(item.createdAt).format(GRAPH_DATE_FORMAT);
+
+        // Increment the count for this log type
+        dateKeyedTypes[timeOfLog][item.type]++;
+    });
+
+    type IGraphableData = ITypesTemp & {
+        date: string;
+    };
+    type IGraphableDataArray = IGraphableData[]; // | undefined[];
+    let graphableData: any[] = [];
+    dateSeries.map((date: string) => {
+        const data = {
+            date: date,
+            ...dateKeyedTypes[date]
+        };
+        graphableData.push(data);
+    });
+
+    return graphableData;
 };
