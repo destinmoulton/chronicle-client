34253fdaecb3127cf61d1bd4cdfb3fa479eb1879
Destin Moulton <destin.moulton@gmail.com>
2018-02-01 17:46:10 -0700

    Added a Select Log Types component.

---
+++ src/components/QueryBar/SelectLogTypes.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { connect } from "react-redux";
+
+import { Select } from "antd";
+const { Option } = Select;
+
+import * as Types from "../../common/types";
+
+interface ISelectLogTypesProps {
+    appLogTypes: Types.TAppLogTypes;
+    selectedAppLogTypes: string[];
+    onSelectLogTypes: (types: string[]) => void;
+}
+
+class SelectLogTypes extends React.Component<ISelectLogTypesProps> {
+    render() {
+        const {
+            appLogTypes,
+            onSelectLogTypes,
+            selectedAppLogTypes
+        } = this.props;
+
+        const options: any = [];
+        appLogTypes.map((type: string) => {
+            options.push(
+                <Option key={type} value={type}>
+                    {type}
+                </Option>
+            );
+        });
+        return (
+            <span>
+                <Select
+                    allowClear={true}
+                    mode="multiple"
+                    value={selectedAppLogTypes}
+                    onChange={onSelectLogTypes}
+                    style={{ width: "100px" }}
+                >
+                    {options}
+                </Select>
+            </span>
+        );
+    }
+}
+
+export default SelectLogTypes;
