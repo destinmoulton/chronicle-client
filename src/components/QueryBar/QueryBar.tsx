8570e98e5cbec6faa9c9c4dcf7b456f8911201c1
Destin Moulton <destin.moulton@gmail.com>
2018-02-01 17:45:25 -0700

    Added SelectLogTypes component.

--- src/components/QueryBar/QueryBar.tsx
+++ src/components/QueryBar/QueryBar.tsx
@@ -6,20 +6,35 @@ import * as Types from "../../common/types";
 
 import DateRange from "./DateRange";
 import RefreshButton from "./RefreshButton";
+import SelectLogTypes from "./SelectLogTypes";
 import SortBy from "./SortBy";
 
 interface IMapStateToProps {
     selectedApp: string;
 }
-interface IQueryBarProps extends IMapStateToProps {}
+interface IQueryBarProps extends IMapStateToProps {
+    appLogTypes: Types.TAppLogTypes;
+    onSelectLogTypes: (types: string[]) => void;
+    selectedAppLogTypes: string[];
+}
 class QueryBar extends React.Component<IQueryBarProps> {
     render() {
+        const {
+            appLogTypes,
+            onSelectLogTypes,
+            selectedAppLogTypes
+        } = this.props;
         return (
             <div className="chc-query-bar">
                 <strong>&nbsp;{this.props.selectedApp}</strong>(<Link to="/">
                     Change App
                 </Link>)&nbsp;&nbsp;
                 <SortBy />&nbsp;&nbsp;
+                <SelectLogTypes
+                    appLogTypes={appLogTypes}
+                    onSelectLogTypes={onSelectLogTypes}
+                    selectedAppLogTypes={selectedAppLogTypes}
+                />
                 <DateRange />&nbsp;&nbsp;
                 <RefreshButton />
             </div>
