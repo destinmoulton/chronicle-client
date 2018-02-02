53eaba32ce60663fc3eed85367b88ca84eb5278a
Destin Moulton <destin.moulton@gmail.com>
2018-02-01 18:19:36 -0700

    Created a sort options constants file for the sort options.

---
+++ src/common/sortoptions.constants.ts
@@ -0,0 +1,12 @@
+/**
+ * Sort Options constants
+ */
+const SORTOPTIONS = [
+    {
+        value: "createdAt:desc",
+        name: "Newest First"
+    },
+    { value: "createdAt:asc", name: "Oldest First" }
+];
+
+export default SORTOPTIONS;
