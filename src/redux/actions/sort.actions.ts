2926f14da3ec084e579917e73366b6d489c6e940
Destin Moulton <destin.moulton@gmail.com>
2018-02-01 17:56:07 -0700

    Removed sort order action.

--- src/redux/actions/sort.actions.ts
+++
@@ -1,8 +0,0 @@
-import * as ActionTypes from "../actionTypes";
-
-export const setSortOrder = (newOrder: string) => {
-    return {
-        type: ActionTypes.SORT_SET_SORT_ORDER,
-        order: newOrder
-    };
-};
