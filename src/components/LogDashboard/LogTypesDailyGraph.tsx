c319eabf952b396eedf8a930bdc6ce18d465fcf3
Destin Moulton <destin.moulton@gmail.com>
2018-02-02 17:20:22 -0700

    Added responsive line component.

--- src/components/LogDashboard/LogTypesDailyGraph.tsx
+++ src/components/LogDashboard/LogTypesDailyGraph.tsx
@@ -1,8 +1,19 @@
 import * as React from "react";
+const ResponsiveLine = require("@nivo/line").ResponsiveLine;
 import { Moment } from "moment";
 
 import * as Types from "../../common/types";
-import { generateDateSeries } from "../../lib/graphprep";
+import { GRAPH_DATE_FORMAT } from "../../common/date.constants";
+import {
+    buildChartableDataArray,
+    generateDateSeries
+} from "../../lib/graphprep";
+
+const CHART_DIM = {
+    WIDTH: 500,
+    HEIGHT: 300,
+    MARGINS: { left: 50, right: 50, top: 25, bottom: 25 }
+};
 
 interface ILogTypesDailyGraphProps {
     appLogs: Types.TAppLogs;
@@ -13,9 +24,65 @@ interface ILogTypesDailyGraphProps {
 const LogTypesDailyGraph: React.SFC<ILogTypesDailyGraphProps> = (
     props: ILogTypesDailyGraphProps
 ) => {
-    const { dateRangeEnd, dateRangeStart } = props;
-    console.log(generateDateSeries(dateRangeStart, dateRangeEnd));
-    return <div />;
+    const { appLogs, appLogTypes, dateRangeEnd, dateRangeStart } = props;
+    const dateSeries = generateDateSeries(dateRangeStart, dateRangeEnd);
+
+    const chartData = buildChartableDataArray(dateSeries, appLogTypes, appLogs);
+
+    return (
+        <div>
+            <ResponsiveLine
+                data={chartData}
+                margin={{
+                    top: 50,
+                    right: 110,
+                    bottom: 50,
+                    left: 60
+                }}
+                minY="auto"
+                stacked={false}
+                axisBottom={{
+                    orient: "bottom",
+                    tickSize: 5,
+                    tickPadding: 5,
+                    tickRotation: 0,
+                    legend: "country code",
+                    legendOffset: 36,
+                    legendPosition: "center"
+                }}
+                axisLeft={{
+                    orient: "left",
+                    tickSize: 5,
+                    tickPadding: 5,
+                    tickRotation: 0,
+                    legend: "count",
+                    legendOffset: -40,
+                    legendPosition: "center"
+                }}
+                dotSize={10}
+                dotColor="inherit:darker(0.3)"
+                dotBorderWidth={2}
+                dotBorderColor="#ffffff"
+                enableDotLabel={true}
+                dotLabel="y"
+                dotLabelYOffset={-12}
+                animate={true}
+                motionStiffness={90}
+                motionDamping={15}
+                legends={[
+                    {
+                        anchor: "bottom-right",
+                        direction: "column",
+                        translateX: 100,
+                        itemWidth: 80,
+                        itemHeight: 20,
+                        symbolSize: 12,
+                        symbolShape: "circle"
+                    }
+                ]}
+            />
+        </div>
+    );
 };
 
 export default LogTypesDailyGraph;
