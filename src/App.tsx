5c63495c3b3fc9d0bf429900ba3f7c4d25d1b286
Destin Moulton <destin.moulton@gmail.com>
2018-02-06 14:50:09 -0700

    Moved the selected app title to the top bar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,30 @@
 import * as React from "react";
-import { connect } from "react-redux";
+
 import { Route } from "react-router";
-import { Link } from "react-router-dom";
 
 import { Layout } from "antd";
 const { Header, Content, Footer } = Layout;
 
 import { API_URL } from "../chronicle.config";
-import * as Types from "./common/types";
 
 import AppSelector from "./components/AppSelector";
 import LogBrowser from "./components/LogBrowser";
 
-interface IMapStateToProps {
-    selectedApp: string;
-}
-interface IAppProps extends IMapStateToProps {}
-class App extends React.Component<IAppProps> {
-    render() {
-        const { selectedApp } = this.props;
+interface IAppProps {}
+const App: React.SFC<IAppProps> = (props: IAppProps) => {
+    return (
+        <Layout className="chc-main-container">
+            <Header className="chc-nav-bar">
+                <div className="chc-nav-title">Chronicle Client</div>
+                <div className="chc-nav-api-url">{API_URL}</div>
+            </Header>
 
-        let app = null;
-        if (selectedApp !== "") {
-            app = (
-                <span>
-                    {selectedApp}(<Link to="/">Change App</Link>)
-                </span>
-            );
-        }
-        return (
-            <Layout className="chc-main-container">
-                <Header className="chc-nav-bar">
-                    <div className="chc-nav-title">Chronicle Client</div>
-                    <div className="chc-nav-title-app">{app}</div>
-                    <div className="chc-nav-api-url">{API_URL}</div>
-                </Header>
-
-                <Content>
-                    <Route exact path="/" component={AppSelector} />
-                    <Route exact path="/browser" component={LogBrowser} />
-                </Content>
-            </Layout>
-        );
-    }
-}
-
-const mapStateToProps = (state: Types.IRootStoreState): IMapStateToProps => {
-    const { query } = state;
-    return {
-        selectedApp: query.selectedApp
-    };
+            <Content>
+                <Route exact path="/" component={AppSelector} />
+                <Route exact path="/browser" component={LogBrowser} />
+            </Content>
+        </Layout>
+    );
 };
 
-export default connect(mapStateToProps)(App);
+export default App;
--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,13 +1,35 @@
 import * as React from "react";
-import DateRange from "./DateRange";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-interface ITopBarProps {}
-const TopBar: React.SFC<ITopBarProps> = (props: ITopBarProps) => {
-    return (
-        <div>
-            <DateRange />
-        </div>
-    );
+import DateRange from "./DateRange";
+import * as Types from "../../common/types";
+
+interface IMapStateToProps {
+    selectedApp: string;
+}
+interface ITopBarProps extends IMapStateToProps {}
+class TopBar extends React.Component<ITopBarProps> {
+    render() {
+        const { selectedApp } = this.props;
+        return (
+            <div className="chc-topbar-container">
+                <div className="chc-topbar-tab">Dashboard</div>
+                <div className="chc-topbar-tab">Browser</div>
+                <div className="chc-topbar-app-name">
+                    {selectedApp}(<Link to="/">Change App</Link>)
+                </div>
+                <DateRange />
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state: Types.IRootStoreState): IMapStateToProps => {
+    const { query } = state;
+    return {
+        selectedApp: query.selectedApp
+    };
 };
 
-export default TopBar;
+export default connect(mapStateToProps)(TopBar);
