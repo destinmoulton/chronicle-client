403e3b6f65530dc02442565555f40e88e50ac72f
Destin Moulton <destin.moulton@gmail.com>
2018-02-06 15:43:53 -0700

    Fixed routing.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import * as React from "react";
 
 import { Route } from "react-router";
 
-import { Layout } from "antd";
+import { Layout, Row, Col } from "antd";
 const { Header, Content, Footer } = Layout;
 
 import { API_URL } from "../chronicle.config";
 
 import AppSelector from "./components/AppSelector";
 import LogBrowser from "./components/LogBrowser";
+
+import LogDashboard from "./components/LogDashboard/LogDashboard";
 
 interface IAppProps {}
 const App: React.SFC<IAppProps> = (props: IAppProps) => {
@@ -20,8 +22,17 @@ const App: React.SFC<IAppProps> = (props: IAppProps) => {
             </Header>
 
             <Content>
-                <Route exact path="/" component={AppSelector} />
-                <Route exact path="/browser" component={LogBrowser} />
+                <Row>
+                    <Col span={24}>
+                        <Route exact path="/" component={AppSelector} />
+                        <Route
+                            exact
+                            path="/dashboard"
+                            component={LogDashboard}
+                        />
+                        <Route exact path="/browser" component={LogBrowser} />
+                    </Col>
+                </Row>
             </Content>
         </Layout>
     );
