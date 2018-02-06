288363c646fb235e717ac58f9442f222e3415725
Destin Moulton <destin.moulton@gmail.com>
2018-02-06 14:22:52 -0700

    Added selectedApp from store to top bar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,55 @@
 import * as React from "react";
+import { connect } from "react-redux";
 import { Route } from "react-router";
+import { Link } from "react-router-dom";
 
 import { Layout } from "antd";
 const { Header, Content, Footer } = Layout;
 
 import { API_URL } from "../chronicle.config";
+import * as Types from "./common/types";
 
 import AppSelector from "./components/AppSelector";
 import LogBrowser from "./components/LogBrowser";
 
-const App: React.SFC = () => {
-    return (
-        <Layout className="chc-main-container">
-            <Header className="chc-nav-bar">
-                <div className="chc-nav-title">Chronicle Client</div>
-                <div className="chc-nav-api-url">{API_URL}</div>
-            </Header>
+interface IMapStateToProps {
+    selectedApp: string;
+}
+interface IAppProps extends IMapStateToProps {}
+class App extends React.Component<IAppProps> {
+    render() {
+        const { selectedApp } = this.props;
 
-            <Content>
-                <Route exact path="/" component={AppSelector} />
-                <Route exact path="/browser" component={LogBrowser} />
-            </Content>
-        </Layout>
-    );
+        let app = null;
+        if (selectedApp !== "") {
+            app = (
+                <span>
+                    {selectedApp}(<Link to="/">Change App</Link>)
+                </span>
+            );
+        }
+        return (
+            <Layout className="chc-main-container">
+                <Header className="chc-nav-bar">
+                    <div className="chc-nav-title">Chronicle Client</div>
+                    <div className="chc-nav-title-app">{app}</div>
+                    <div className="chc-nav-api-url">{API_URL}</div>
+                </Header>
+
+                <Content>
+                    <Route exact path="/" component={AppSelector} />
+                    <Route exact path="/browser" component={LogBrowser} />
+                </Content>
+            </Layout>
+        );
+    }
+}
+
+const mapStateToProps = (state: Types.IRootStoreState): IMapStateToProps => {
+    const { query } = state;
+    return {
+        selectedApp: query.selectedApp
+    };
 };
 
-export default App;
+export default connect(mapStateToProps)(App);
