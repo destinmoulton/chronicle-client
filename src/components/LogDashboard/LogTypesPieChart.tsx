30e874c30c5ece3769315f7b92d826e4d55c3553
Destin Moulton <destin.moulton@gmail.com>
2018-02-15 14:42:27 -0700

    Removed div container.

--- src/components/LogDashboard/LogTypesPieChart.tsx
+++ src/components/LogDashboard/LogTypesPieChart.tsx
@@ -27,44 +27,42 @@ const LogTypesPieChart = (props: ILogTypesDailyGraphProps) => {
         props.appLogs
     );
     return (
-        <div>
-            <Pie
-                data={graphableData}
-                width={CHART_DIM.WIDTH}
-                height={CHART_DIM.HEIGHT}
-                margin={{ ...CHART_DIM.MARGINS }}
-                innerRadius={0.5}
-                padAngle={0.7}
-                cornerRadius={3}
-                colors="d320c"
-                colorBy="id"
-                borderColor="inherit:darker(0.6)"
-                radialLabelsSkipAngle={10}
-                radialLabelsTextXOffset={6}
-                radialLabelsTextColor="#333333"
-                radialLabelsLinkOffset={0}
-                radialLabelsLinkDiagonalLength={16}
-                radialLabelsLinkHorizontalLength={24}
-                radialLabelsLinkStrokeWidth={1}
-                radialLabelsLinkColor="inherit"
-                slicesLabelsSkipAngle={10}
-                slicesLabelsTextColor="#333333"
-                animate={true}
-                motionStiffness={90}
-                motionDamping={15}
-                legends={[
-                    {
-                        anchor: "bottom",
-                        direction: "row",
-                        translateY: 40,
-                        itemWidth: 80,
-                        itemHeight: 14,
-                        symbolSize: 14,
-                        symbolShape: "circle"
-                    }
-                ]}
-            />
-        </div>
+        <Pie
+            data={graphableData}
+            width={CHART_DIM.WIDTH}
+            height={CHART_DIM.HEIGHT}
+            margin={{ ...CHART_DIM.MARGINS }}
+            innerRadius={0.5}
+            padAngle={0.7}
+            cornerRadius={3}
+            colors="d320c"
+            colorBy="id"
+            borderColor="inherit:darker(0.6)"
+            radialLabelsSkipAngle={10}
+            radialLabelsTextXOffset={6}
+            radialLabelsTextColor="#333333"
+            radialLabelsLinkOffset={0}
+            radialLabelsLinkDiagonalLength={16}
+            radialLabelsLinkHorizontalLength={24}
+            radialLabelsLinkStrokeWidth={1}
+            radialLabelsLinkColor="inherit"
+            slicesLabelsSkipAngle={10}
+            slicesLabelsTextColor="#333333"
+            animate={true}
+            motionStiffness={90}
+            motionDamping={15}
+            legends={[
+                {
+                    anchor: "bottom",
+                    direction: "row",
+                    translateY: 40,
+                    itemWidth: 80,
+                    itemHeight: 14,
+                    symbolSize: 14,
+                    symbolShape: "circle"
+                }
+            ]}
+        />
     );
 };
 
