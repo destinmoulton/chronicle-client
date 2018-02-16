15183e10fad9b7e0b1919742e70d6b16c96035f4
Destin Moulton <destin.moulton@gmail.com>
2018-02-16 13:16:00 -0700

    Created nivo constants file with the pie chart settings.

---
+++ src/common/nivo.constants.ts
@@ -0,0 +1,43 @@
+/**
+ * Constants for the nivo react components
+ */
+
+export const PIE = {
+    BORDER_COLOR: "inherit:darker(0.6)",
+    COLORS: "d320c",
+    COLOR_BY: "id",
+    CORNER_RADIUS: 3,
+    HEIGHT: 400,
+    INNER_RADIUS: 0.5,
+    PAD_ANGLE: 0.7,
+    LEGEND: {
+        anchor: "bottom-right",
+        direction: "column",
+        translateX: 110,
+        itemWidth: 100,
+        itemHeight: 14,
+        symbolSize: 14,
+        symbolShape: "circle"
+    },
+    MARGINS: {
+        top: 20,
+        right: 100,
+        bottom: 20,
+        left: 80
+    },
+    RADIAL_LABELS: {
+        SkipAngle: 10,
+        TextXOffset: 6,
+        TextColor: "#333333",
+        LinkOffset: 0,
+        LinkDiagonalLength: 16,
+        LinkHorizontalLength: 24,
+        LinkStrokeWidth: 1,
+        LinkColor: "inherit"
+    },
+    SLICES_LABELS: {
+        SkipAngle: 10,
+        TextColor: "#333333"
+    },
+    WIDTH: 400
+};
